013a8ba9063d0208e6d4f94feb3a5d446a549807
Sara Farazi <s.farazi90@gmail.com>
2018-11-30 12:12:19 -0500

    changes added to server

--- main.py
+++ main.py
@@ -1,41 +1,115 @@
 import os
 import pdb
+import json
 from subprocess import call as proccall
 from flask import Flask
 from flask import request
+from flask_cors import CORS
 
 
 app = Flask("NetCDF-Server")
+CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 def parse_json(obj):
-	pass
+	all_coordinates = obj["coordinates"]
+	geo_type = obj["type"]
+	top_left = all_coordinates[0]
+	top_right = all_coordinates[1]
+	bottom_right = all_coordinates[2]
+	bottom_left = all_coordinates[3]
+	center = all_coordinates[4]
 
+	top_left_lon = top_left[0]
+	bottom_left_lon = bottom_left[0]
+
+	top_right_lon = top_right[0]
+	bottom_right_lon = bottom_right[0]
+
+	# min_lon = min(top_left_lon, bottom_left_lon)
+	# max_lon = max(top_right_lon, bottom_right_lon)
+
+	top_left_lat = top_left[1]
+	bottom_left_lat = bottom_left[1]
+
+	top_right_lat = top_right[1]
+	bottom_right_lat = bottom_right[1]
+
+	# min_lat = min(bottom_right_lat, bottom_left_lat)
+	# max_lat = max(top_right_lat, top_left_lat)
+	geojson = {
+			  "type": "FeatureCollection",
+			  "features": [
+			    {
+			      "type": "Feature",
+			      "geometry": {
+			        "type": geo_type,
+			        "coordinates": [
+			          [
+			            [
+			              top_left_lon,
+			              top_left_lat
+			            ],
+			            [
+			              top_right_lon,
+			              top_right_lat
+			            ],
+			            [
+			              bottom_right_lon,
+			              bottom_right_lat
+			            ],
+			            [
+			              bottom_left_lon,
+			              bottom_left_lat
+			            ],
+			            [
+			              center[0],
+			              center[1]
+			            ]
+			          ]
+			        ]
+			      }
+			    }
+			  ]
+			}
+	with open("geojson.json", w) as f:
+		f.write(json.dumps(geojson))
+
+	return geojson
 
 
 @app.route('/')
 def main():
 	if request.method == 'GET': 
-		return 'First page'
+		min_lat = 38.2270393371582
+		min_lon = -95.11832427978516
+		max_lat = 51.424442291259766
+		max_lon = -71.68765258789062
+		
+		bounding_box = {
+			"data": "Five Great Lakes",
+			"min_lon": min_lon,
+			"min_lat": min_lat,
+			"max_lon": max_lon,
+			"max_lat": max_lat,
+		}
+		return json.dumps(bounding_box)
+
 	elif request.method == 'POST':
 		# data is supposed to be a JSON object
 		# make the geoJson polygon and pass it to process call
 		data = parse_json(request.get_json())
 		if data:
-			# proccall([
-			# 	'cd',
-			# 	basedir + '../geotrellis-netcdf'
-			# ])
 			proccall([
 				'spark-submit',
 				'--master',
-				'\'local[*]\'',
+				'local[*]',
 				'gddp/target/scala-2.11/gddp-assembly-0.22.7.jar',
-				'/tmp/gddp.nc',
-				# geojson here
+				'data/test.nc',
+				'geojson.json'
 			])
 			# process completed.
-
+			return send_file('gddp1.png', mimetype='image/png')
 			pass
 		else: 
 			return '{message: "Server Error"}'
