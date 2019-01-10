0621384b0b059ed52a0d7251fffc419d52560cd4
ubekz <martin.gauch@student.kit.edu>
2019-01-10 11:28:36 -0500

    Start python version

--- geoPy/geopy.py
+++ geoPy/geopy.py
@@ -1,114 +1,146 @@
-import os
+from os import listdir
+from os.path import isfile, join
 import sys
-import pdb
-import math
-from netCDF4 import Dataset
-from pyspark import SparkContext, SparkConf
+from netCDF4 import Dataset, date2index
+from pyspark import SparkContext
+from dateutil.parser import parse
+from datetime import time, timedelta, datetime
+import geopyspark as gps
 
-# This code does the same thing as gddp.scala. Excpet that it cannot create a PNG file and currently it is not using spark.
-# You need to install PySpark and GeoPySpark and netCDF4 in order to run this code.
 
-y_array = None
-x_array = None
-lat_array = None
-lon_array = None
-total_temp = None
-temperature = None
+def open_file(path):
+    return Dataset(path, "r", format="NETCDF4")
 
-def openFile(path):
-	return Dataset(path, "r", format="NETCDF4")
 
-def getIndexes(lat, lon):
-	global lat_array, lon_array
-	minDiffLatIndex = -1
-	minDiffLat = sys.maxsize
+def get_indexes(lat_array, lon_array, shape, lat, lon):
+    flattened_lat = lat_array[:].flatten()
+    flattened_lon = lon_array[:].flatten()
+    min_pow_dis = sys.maxsize
+    index = 0
+    for i in range(0, len(flattened_lat)):
+        pow_dis = pow(flattened_lat[i] - lat, 2) + pow(flattened_lon[i] - lon, 2)
+        if pow_dis < min_pow_dis:
+            min_pow_dis = pow_dis
+            index = i
 
-	for i, latItem in enumerate(lat_array.flatten()):
-		if abs(latItem - lat) < minDiffLat:
-			minDiffLat = abs(latItem - lat)
-			minDiffLatIndex = i
+    return int(index / shape[1]), index % shape[1]  # y,x
 
-	minDiffLonIndex = -1
-	minDiffLon = sys.maxsize
 
-	for i, lonItem in enumerate(lon_array.flatten()):
-		if abs(lonItem - lon) < minDiffLon:
-			minDiffLon = abs(lonItem - lon)
-			minDiffLonIndex = i
+# Transforms a lat/lon-polygon to x/y-coordinates
+def get_bounding_box_polygon(lat_array, lon_array, shape, polygon_extent):
+    # todo: verify correctness
+    y_slice_start, x_slice_start = get_indexes(lat_array, lon_array, shape, polygon_extent.xmin, polygon_extent.ymin)
+    y_slice_stop, x_slice_stop = get_indexes(lat_array, lon_array, shape, polygon_extent.xmax, polygon_extent.ymax)
 
-	return [
-		int(math.floor(minDiffLatIndex / 1178)),
-		int(math.floor(minDiffLatIndex % 1178)),
-		int(math.floor(minDiffLonIndex / 1178)),
-		int(math.floor(minDiffLonIndex % 1178)),
-	]
+    print("{} {}".format(x_slice_start, x_slice_stop))
+    print("{} {}".format(y_slice_start, y_slice_stop))
 
-	
+    return x_slice_start, x_slice_stop, y_slice_start, y_slice_stop
 
-def readInput(path):
-	global total_temp, x_array, y_array, lon_array, lat_array, temperature
-	data = openFile(path)
-	temperature = data.get_variables_by_attributes(long_name="daily average temperature")[0]
-	lat = data.get_variables_by_attributes(long_name="latitude coordinate")[0]
-	lon = data.get_variables_by_attributes(long_name="longitude coordinate")[0]
-	x = data.get_variables_by_attributes(long_name="x coordinate of projection")[0]
-	y = data.get_variables_by_attributes(long_name="y coordinate of projection")[0]
 
-	total_temp = temperature[:][0]
-	lat_array = lat[:]
-	lon_array = lon[:]
-	x_array = x[:]
-	y_array = y[:]
+def read_input(path):
+    return open_file(path)
 
 
 def slice(array, xstart, xend, ystart, yend):
-	result = []
+    result = []
 
-	for item in array[xstart:xend]:
-		result.append(item[ystart:yend])
+    for item in array[xstart:xend]:
+        result.append(item[ystart:yend])
 
-	# pdb.set_trace()
-	return result
-
-
-def process_query(latMin, latMax, lonMin, lonMax):
-
-	global total_temp, x_array, y_array, lon_array, lat_array
-	geopy.readInput('data/test.nc') 
-
-	PolyMinIndexes = getIndexes(latMin, lonMin)
-	PolyMaxIndexes = getIndexes(latMax, lonMax)
-
-	ySliceStart = min(min(PolyMaxIndexes[1], PolyMinIndexes[1]), min(PolyMaxIndexes[3], PolyMinIndexes[3]))
-	ySliceStop = max(max(PolyMaxIndexes[1], PolyMinIndexes[1]), max(PolyMaxIndexes[3], PolyMinIndexes[3]))
-	xSliceStart = min(min(PolyMaxIndexes[0], PolyMinIndexes[0]), min(PolyMaxIndexes[2], PolyMinIndexes[2]))
-	xSliceStop = max(max(PolyMaxIndexes[0], PolyMinIndexes[0]), max(PolyMaxIndexes[2], PolyMinIndexes[2]))
-
-	xSize = xSliceStop - xSliceStart + 1
-	ySize = ySliceStop - ySliceStart + 1
-
-	# conf = SparkConf()
-	# conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-	# conf.set("spark.kryo.registrator", "geotrellis.spark.io.kryo.KryoRegistrator")
-	# conf.set("spark.kryo.unsafe", "true")
-	# conf.set("spark.rdd.compress", "true")
-	# conf.set("spark.ui.enabled", "false")
-	# sc = SparkContext(conf=conf)
-
-	# Currently it is reading one file (one day) and we don't need spark, I commented out the spark conf building. If we want to use pyspark, you should use a map or flat map for reading several files or creating any other RDDs.
-	sliced_temp = slice(total_temp, xSliceStart, xSliceStop, ySliceStart, ySliceStop)
-	
-	for item in sliced_temp[:]:
-		item.dump("result.txt")
-			
+    return result
 
 
+def process_query(lat_min, lat_max, lon_min, lon_max, date_range, request_variables):
+    request_vars = request_variables.split(',')
 
-def main():
-	global total_temp, x_array, y_array, lon_array, lat_array
-	path = "../data/test.nc"
-	readInput(path)
+    nc_base_path = 'data'
+    nc_files = [nc_base_path + '/' + f for f in listdir(nc_base_path) if
+                isfile(join(nc_base_path, f)) and f.endswith('.nc')]
 
+    request_date_range = date_range.split(",")
+    request_start_date = parse(request_date_range[0]).date()
+    request_end_date = parse(request_date_range[1]).date()
+    print("Request time range: {} - {}".format(request_start_date, request_end_date))
+    print("Request variables: {}".format(request_vars))
 
-if __name__ == "__main__":
-	main()
+    files_to_analyze = dict()
+    for f in nc_files:
+        nc_file = open_file(f)
+        file_vars = nc_file.variables.keys()
+        print("File {} has variables {}".format(f, file_vars))
+
+        file_time_range = nc_file['time'][:]
+        file_start_date = (datetime(1990, 1, 1, 0, 0) + timedelta(hours=float(file_time_range.min()))).date()
+        file_end_date = (datetime(1990, 1, 1, 0, 0) + timedelta(hours=float(file_time_range.max()))).date()
+        print("File has time range {} - {}".format(file_start_date, file_end_date))
+
+        if file_start_date <= request_end_date and file_end_date >= request_start_date:
+            contained_vars = set(request_vars).intersection(file_vars)
+            for v in contained_vars:
+                if v not in files_to_analyze:
+                    files_to_analyze[v] = [nc_file]
+                else:
+                    files_to_analyze[v] = files_to_analyze[v].append(nc_file)
+
+    print("Matching files: {}".format(files_to_analyze))
+
+    conf = gps.geopyspark_conf(appName="gwf", master="local[*]")
+    conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    conf.set("spark.kryo.registrator", "geotrellis.spark.io.kryo.KryoRegistrator")
+    conf.set("spark.kryo.unsafe", "true")
+    conf.set("spark.rdd.compress", "true")
+    conf.set("spark.ui.enabled", "false")
+    sc = SparkContext(conf=conf)
+
+    polygon_extent = gps.Extent(lat_min, lon_min, lat_max, lon_max)
+
+    def spark_process(var_name, nc_file_list):
+        print("variable: {}, files {}".format(var_name, nc_file_list))
+
+        # At some point, we'll want to support partitioned files. For now, just take the first one.
+        nc_file = nc_file_list[0]
+        vs = nc_file.variables
+
+        lat_array = nc_file['lat']
+        lon_array = nc_file['lon']
+
+        no_data = nc_file[var_name].getncattr('_FillValue')
+
+        # Get x/y-range
+        (xSliceStart, xSliceStop, ySliceStart, ySliceStop) = get_bounding_box_polygon(lat_array, lon_array,
+                                                                                              lon_array.shape,
+                                                                                              polygon_extent)
+        x = xSliceStop - xSliceStart + 1
+        y = ySliceStop - ySliceStart + 1
+        print("x: {}, y: {}, {}".format(x, y, (xSliceStart, xSliceStop, ySliceStart, ySliceStop)))
+
+        # Get indices of the request's time range
+        start_time_index, end_time_index = date2index([datetime.combine(request_start_date, time(0, 0)),
+                                                       datetime.combine(request_end_date, time(0, 0))],
+                                                      nc_file['time'])
+
+        print("time slice indices: {} - {}".format(start_time_index, end_time_index))
+
+        # Read the section specified by the request (i.e. specified time and x/y-location)
+        array = nc_file[var_name][start_time_index:end_time_index+1, ySliceStart:ySliceStop+1, xSliceStart:xSliceStop+1]
+        print("array 1st 10 entries: {}".format(array[:10]))
+
+        proj = nc_file['lambert_azimuthal_equal_area'].getncattr('crs_wkt')
+        instant = datetime.combine(request_start_date, time(0, 0))
+        bounds = gps.Bounds(gps.SpaceTimeKey(col=0, row=0, instant=instant),
+                            gps.SpaceTimeKey(col=0, row=0, instant=instant))
+        layout=gps.TileLayout(1, 1, x, y)
+        layout_definition = gps.LayoutDefinition(polygon_extent, layout)
+        metadata = gps.Metadata(bounds=bounds, crs='+proj=longlat +datum=WGS84 +no_defs ',
+                                cell_type='float32ud-1.0', extent=polygon_extent, layout_definition=layout_definition)
+        tile = gps.Tile.from_numpy_array(array, no_data)
+
+        layer = [(gps.SpatialKey(row=0, col=0), tile)]
+        rdd = sc.parallelize(layer)
+
+        result_tile = gps.TiledRasterLayer.from_numpy_rdd(layer_type=gps.LayerType.SPATIAL,
+                                                          numpy_rdd=rdd, metadata=metadata)
+
+    for v in request_vars:
+        spark_process(v, files_to_analyze[v])
