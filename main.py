93aa3abcd07de6c93f6e11ab9e4e2b43fb8fb0eb
Yixin Wang <ywang@sprypoint.com>
2019-01-10 15:37:28 -0500

    add vars

--- main.py
+++ main.py
@@ -18,105 +18,106 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 def parse_json(obj):
 
-	daterange = "2002-10-01,2002-10-01" #obj["date"]
-	variables = "LST_LWST_avg_daily,LST_LWST_avg_day" #obj["variables"]
+     daterange = obj["selectDate"]
+     variables = obj["variables"]
+     geo = obj["geoJson"]
 
-	all_coordinates = obj["coordinates"][0]
-	geo_type = obj["type"]
-	top_left = all_coordinates[0]
-	top_right = all_coordinates[1]
-	bottom_right = all_coordinates[2]
-	bottom_left = all_coordinates[3]
-	center = all_coordinates[4]
+     all_coordinates = geo["coordinates"][0]
+     geo_type = geo["type"]
+     top_left = all_coordinates[0]
+     top_right = all_coordinates[1]
+     bottom_right = all_coordinates[2]
+     bottom_left = all_coordinates[3]
+     center = all_coordinates[4]
 
-	top_left_lon = top_left[0]
-	bottom_left_lon = bottom_left[0]
+     top_left_lon = top_left[0]
+     bottom_left_lon = bottom_left[0]
 
-	top_right_lon = top_right[0]
-	bottom_right_lon = bottom_right[0]
+     top_right_lon = top_right[0]
+     bottom_right_lon = bottom_right[0]
 
-	min_lon = min(top_left_lon, bottom_left_lon)
-	max_lon = max(top_right_lon, bottom_right_lon)
+     min_lon = min(top_left_lon, bottom_left_lon)
+     max_lon = max(top_right_lon, bottom_right_lon)
 
-	top_left_lat = top_left[1]
-	bottom_left_lat = bottom_left[1]
+     top_left_lat = top_left[1]
+     bottom_left_lat = bottom_left[1]
 
-	top_right_lat = top_right[1]
-	bottom_right_lat = bottom_right[1]
+     top_right_lat = top_right[1]
+     bottom_right_lat = bottom_right[1]
 
-	min_lat = min(bottom_right_lat, bottom_left_lat)
-	max_lat = max(top_right_lat, top_left_lat)
-	geojson = {
-			  "type": "FeatureCollection",
-			  "features": [
-			    {
-			      "type": "Feature",
-			      "geometry": {
-			        "type": geo_type,
-			        "coordinates": [
-			          [
-			            [
-			              top_left_lon,
-			              top_left_lat
-			            ],
-			            [
-			              top_right_lon,
-			              top_right_lat
-			            ],
-			            [
-			              bottom_right_lon,
-			              bottom_right_lat
-			            ],
-			            [
-			              bottom_left_lon,
-			              bottom_left_lat
-			            ],
-			            [
-			              center[0],
-			              center[1]
-			            ]
-			          ]
-			        ]
-			      }
-			    }
-			  ]
-			}
-	with open("geojson.json", 'w') as f:
-		f.write(json.dumps(geojson))
+     min_lat = min(bottom_right_lat, bottom_left_lat)
+     max_lat = max(top_right_lat, top_left_lat)
+     geojson = {
+                 "type": "FeatureCollection",
+                 "features": [
+                   {
+                     "type": "Feature",
+                     "geometry": {
+                       "type": geo_type,
+                       "coordinates": [
+                         [
+                           [
+                             top_left_lon,
+                             top_left_lat
+                           ],
+                           [
+                             top_right_lon,
+                             top_right_lat
+                           ],
+                           [
+                             bottom_right_lon,
+                             bottom_right_lat
+                           ],
+                           [
+                             bottom_left_lon,
+                             bottom_left_lat
+                           ],
+                           [
+                             center[0],
+                             center[1]
+                           ]
+                         ]
+                       ]
+                     }
+                   }
+                 ]
+               }
+     with open("geojson.json", 'w') as f:
+          f.write(json.dumps(geojson))
 
-	return (daterange, variables)
-	#return [min_lat, max_lat, min_lon, max_lon]
+     return (daterange, variables)
+     #return [min_lat, max_lat, min_lon, max_lon]
 
 
 @app.route('/getBoundary',methods = ['GET'])
 def getBoundary():
-	boundary = [[41.0270393371582,-93.11832427978516], [49.424442291259766, -75.28765258789062]]
-	return jsonify(boundary)
+     boundary = [[41.0270393371582,-93.11832427978516], [49.424442291259766, -75.28765258789062]]
+     return jsonify(boundary)
 
 @app.route('/fetchResult',methods = ['POST'])
 def fetchResult():
-	daterange, variables = parse_json(request.get_json())
-	# Calling the Python code is commented out
-	# geopy.process_query(data[0], data[1], data[2], data[3])
-	print(request.get_json())
-	if daterange:
-		proccall(
-			"spark-submit.cmd --master local gddp/target/scala-2.11/gddp-assembly-0.22.7.jar data geojson.json "
-				+ daterange + " " + variables,
-			shell = True
-		)
-		# process completed.
+     daterange, variables = parse_json(request.get_json())
+     # Calling the Python code is commented out
+     # geopy.process_query(data[0], data[1], data[2], data[3])
+     print(request.get_json())
+     if daterange:
+          proccall(
+               "spark-submit --master 'local[*]' gddp/target/scala-2.11/gddp-assembly-0.22.7.jar data geojson.json "
+                    + daterange + " " + variables,
+               shell = True
+          )
+          # process completed.
 
-		rv = None
-		for v in variables.split(","):
-			rv = send_file('gddp' + v + daterange.replace(',','-') + '.png', mimetype='image/png')
-		return rv
-		# Use the below line if you want to return the result of Python code.
-		# return send_file('result.txt', mimetype='text/csv')
-		pass
-	else: 
-		return '{message: "Server Error"}'
+          rv = None
+          for v in variables.split(","):
+               rv = send_file('gddp' + v + daterange.replace(',','-') + '.png', mimetype='image/png')
+          return rv
+          # Use the below line if you want to return the result of Python code.
+          # return send_file('result.txt', mimetype='text/csv')
+          pass
+     else: 
+          return '{message: "Server Error"}'
 
 
 if __name__ == '__main__':
-	app.run()
+     app.run()
