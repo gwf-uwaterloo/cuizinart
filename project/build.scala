19327400377debd56f1bc19fba6949e171000427
ubekz <martin.gauch@student.kit.edu>
2019-01-10 11:35:47 -0500

    Remove scala files

--- project/build.scala
+++
@@ -1,9 +0,0 @@
-import sbt._
-import scala.util.Properties
-
-object Version {
-  def either(environmentVariable: String, default: String): String = Properties.envOrElse(environmentVariable, default)
-
-  lazy val hadoop  = either("SPARK_HADOOP_VERSION", "2.7.3")
-  lazy val spark   = either("SPARK_VERSION", "2.1.0")
-}
