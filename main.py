6414cc40677ace91f57b05c4dafffa6478ba765e
Yixin Wang <ywang@sprypoint.com>
2019-01-10 21:02:38 -0500

    delete unused code

--- main.py
+++ main.py
@@ -18,32 +18,6 @@ app = Flask("NetCDF-Server")
 CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
-def compress(file_names):
-    print("File Paths:")
-    print(file_names)
-
-    path = "C:/data/"
-
-    # Select the compression mode ZIP_DEFLATED for compression
-    # or zipfile.ZIP_STORED to just store the file
-    compression = zipfile.ZIP_DEFLATED
-
-    # create the zip file first parameter path/name, second mode
-    zf = zipfile.ZipFile("RAWs.zip", mode="w")
-    try:
-        for file_name in file_names:
-            # Add file to the zip file
-            # first parameter file to zip, second filename in zip
-            zf.write(path + file_name, file_name, compress_type=compression)
-
-    except FileNotFoundError:
-        print("An error occurred")
-    finally:
-        # Don't forget to close the file!
-        zf.close()
-
-
 def parse_json(obj):
     daterange = obj["selectDate"]
     variables = obj["variables"]
