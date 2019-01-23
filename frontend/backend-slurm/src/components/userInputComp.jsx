befd454a1e594cf811c4c8d43fc87c6fc5b8b26c
Martin Gauch <15731649+gauchm@users.noreply.github.com>
2019-01-22 19:31:00 -0500

    Merge graham into master (#6)
    * update map bbox
    
    * Block requests for invalid users
    
    * Include user in request files
    
    * fix missing product
    
    * change process behaviour
    
    * update name
    
    * write request file with double quotes
    
    * add get boudaries api
    
    * Fix path to boundaries files
    
    * Return shape as lat/lon instead of lon/lat
    
    * Handle quotes around json string
    
    * send list, not np array to frontend
    
    * Fix lat/lon swap
    
    * Fix path
    
    * Check for .info extension
    
    * add bbox
    
    * upload logos
    
    * add date validation
    
    * add more validation
    
    * add date validation
    
    * hard code the date range for now
    
    * change color
    
    * Make master ready to merge with graham
    
    * Make graham ready to merge with master

---
+++ frontend/backend-slurm/src/components/userInputComp.jsx
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import DateRangePicker from 'react-bootstrap-daterangepicker';
+import Select from 'react-select';
+import moment from 'moment';
+/*
+    show date range picker, headers checkbox
+ */
+
+export default class UserInputComp extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        selectedProduct: null
+    };
+
+    handleDateEvent(event, picker) {
+        this.props.updateUserInputs({start_time: picker.startDate.format("YYYY-MM-DD"), end_time: picker.endDate.format("YYYY-MM-DD")});
+    }
+
+    handleSelectProduct = (selectedOption) => {
+        this.setState({selectedProduct: selectedOption});
+        this.props.updateDateSet({selectDateSet: selectedOption});
+    };
+
+    handleChange = (key, event) => {
+        this.props.updateUserInputs({[key]: event.target.value});
+    };
+
+    handleInvalidDate = (date) => {
+        if(this.state.selectedProduct){
+            if (moment(date).isBefore(this.state.selectedProduct.valid_start_time) || moment(date).isAfter(this.state.selectedProduct.valid_end_time)) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    render() {
+        return (
+            <div>
+                <form className="row">
+                    <div className="form-group col-lg-3">
+                        <label htmlFor="product">Select a Product</label>
+                        <Select
+                            id="product"
+                            value={this.state.selectedProduct}
+                            placeholder={"Choose product..."}
+                            onChange={this.handleSelectProduct}
+                            options={this.props.products}
+                        />
+                    </div>
+                    <div className="form-group col-lg-3">
+                        <label htmlFor="userEmail">Email</label>
+                        <input type="email" className="form-control" id="userEmail" aria-describedby="emailHelp"
+                               placeholder="Enter email"  onChange={this.handleChange.bind(this, "user_email")}/>
+                    </div>
+                    <div className="form-group col-lg-3">
+                        <label htmlFor="userId">User ID</label>
+                        <input type="text" className="form-control" id="userId"
+                               placeholder="Enter User ID"  onChange={this.handleChange.bind(this, "user_id")}/>
+                    </div>
+                    <div className="form-group col-lg-3">
+                        <label htmlFor="requestId">Request ID</label>
+                        <input type="text" className="form-control" id="requestId"
+                               placeholder="Enter Request ID"  onChange={this.handleChange.bind(this, "request_id")}/>
+                    </div>
+                </form>
+                <DateRangePicker  startDate={this.state.selectedProduct? moment(this.state.selectedProduct.valid_start_time).format("MM/DD/YYYY") : moment().format("MM/DD/YYYY")}
+                                 isInvalidDate={(date) => this.handleInvalidDate(date)}
+                                 showDropdowns onApply={(e, picker) => this.handleDateEvent(e, picker)}>
+                    <button className="btn btn-info ">Select Date</button>
+                </DateRangePicker>
+            </div>
+
+        );
+    }
+}
+
