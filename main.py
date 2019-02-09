d90b4c6873e3c6d7d314ed7777b4430b5a30cf47
Martin Gauch <15731649+gauchm@users.noreply.github.com>
2019-02-09 00:35:32 -0500

    Backend facade (#15)
    * Update path where json is sent
    
    * Backend work for #2: Create a unified endpoint facade to be accessed no matter which backend (slurm/pyspark) is being used
    
    * update userId to globusId

--- main.py
+++ main.py
@@ -7,40 +7,39 @@ from flask import request
 from flask_cors import CORS
 from flask import jsonify
 from flask import send_file
-import zipfile
 import netCDF4  # for strange reasons, one has to import netCDF4 before geopyspark on some systems, otherwise reading nc-files fails.
 import geopyspark as gps
 from pyspark import SparkContext
 
 from geoPy import geopy
 
-app = Flask("NetCDF-Server")
+
+BACKEND_SLURM = 'slurm'
+BACKEND_PYSPARK = 'pyspark'
+
+app = Flask('Cuizinart')
 CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-conf = gps.geopyspark_conf(appName="gwf", master="local[*]")
+conf = gps.geopyspark_conf(appName='gwf', master='local[*]')
 sc = SparkContext(conf=conf)
 
 product_dict = {}
 
+
 def parse_json(obj):
-    product = obj["product"]
-    start_time = obj["start_time"]
-    end_time = obj["end_time"]
-    request_variables = obj["variables"]
-    geojson = obj["bounding_geom"]
+    backend = obj['backend']
+    product = obj['product']
+    start_time = obj['start_time']
+    end_time = obj['end_time']
+    request_variables = obj['variables']
+    geojson = obj['bounding_geom']
 
-    return product, geojson, start_time, end_time, request_variables
-
-
-@app.route('/getBoundary', methods=['GET'])
-def getBoundary():
-    boundary = [[41.0270393371582, -93.11832427978516], [49.424442291259766, -75.28765258789062]]
-    return jsonify(boundary)
+    return backend, product, geojson, start_time, end_time, request_variables
 
 
 @app.route('/getBoundaries', methods=['GET'])
-def getBoundaries():
+def get_boundaries():
     files = os.listdir('data/boundaries')
     files_json = []
     for file_name in files:
@@ -65,10 +64,28 @@ def getBoundaries():
 
 
 @app.route('/fetchResult', methods=['POST'])
-def fetchResult():
-    print(request.get_json())
+def fetch_result():
+    """
+    This is the main REST endpoint. It receives the processing request as a JSON string.
+    Depending on the specified backend, it passes the request on to be processed by Slurm or PySpark.
+    """
+    json_request = request.get_json()
+    print(json_request)
 
-    product, geojson, start_time, end_time, variables = parse_json(request.get_json())
+    backend, product, geojson, start_time, end_time, variables = parse_json(json_request)
+
+    if backend == BACKEND_SLURM:
+        return process_slurm(json_request)
+    elif backend == BACKEND_PYSPARK:
+        return process_pyspark(product, geojson, start_time, end_time, variables)
+    else:
+        return '{message: "Unknown Backend {}"}'.format(backend), 400
+
+
+def process_pyspark(product, geojson, start_time, end_time, variables):
+    """
+    Process request using PySpark.
+    """
     input_file_name = product_dict[product]
     try:
         out_file_name = geopy.process_query(input_file_name, geojson, start_time, end_time, variables, sc)
@@ -85,7 +102,27 @@ def fetchResult():
         finally:
             return rv
     else:
-        return '{message: "Server Error"}'
+        return '{message: "Server Error"}', 500
+
+
+def process_slurm(json_request):
+    """
+    scp the request json to Graham, where it will be processed.
+    """
+    request_string = str(json_request).replace("'", '"')
+
+    file_name = '__cuizinart-graham-request-{}-{}.dat'.format(json_request['globus_id'],
+                                                              json_request['request_id'])
+    with open(file_name, 'w') as f:
+        f.write(request_string)
+
+    os.system(
+        'scp -i "~/.ssh/id_rsa_graham" {} mgauch@graham.computecanada.ca:/project/6008034/cuizinart/INBOX/'.format(
+            file_name))
+
+    os.remove(file_name)
+
+    return '{message: "success"}'
 
 
 if __name__ == '__main__':
