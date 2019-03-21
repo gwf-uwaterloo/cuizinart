915384de9d63e2fdc00e29200801cd5c51fdb52c
YIXIN-WANG <43051742+YIXIN-WANG@users.noreply.github.com>
2019-03-21 19:22:11 -0400

    Monitor (#30)
    * monitor directory and file changes
    * update meta db schema
    * add constraint to nc filename

--- cuizinart/metadata_schema.py
+++ cuizinart/metadata_schema.py
@@ -1,10 +1,10 @@
 import os
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from marshmallow import fields
 from flask_marshmallow import Marshmallow
 from settings import *
+from flask_migrate import Migrate, MigrateCommand
 
 
 app = Flask('cuizinart')
@@ -31,12 +31,16 @@ class Product(db.Model):
     variables = db.relationship('Variable', backref='product', lazy=True)
     horizons = db.relationship('Horizon', backref='product', lazy=True)
     issues = db.relationship('Issue', backref='product', lazy=True)
+    ncFiles = db.relationship('NCFile', backref='product', lazy=True)
 
     def __repr__(self):
         return '<Product {!r}>'.format(self.key)
 
 
 class Variable(db.Model):
+    __table_args__ = (
+        db.UniqueConstraint('key', 'product_id', name='variable_uc'),
+    )
     variable_id = db.Column(db.Integer, primary_key=True)
     key = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
@@ -47,7 +51,6 @@ class Variable(db.Model):
     level = db.Column(db.String)
     unit = db.Column(db.String)
 
-    db.UniqueConstraint('key', 'product_id', name='variable_uc')
 
     def __repr__(self):
         return '<Variable {!r} (product: {!r})>'.format(self.key, self.product_id)
@@ -56,8 +59,8 @@ class Variable(db.Model):
 class Domain(db.Model):
     domain_id = db.Column(db.Integer, primary_key=True)
     extent = db.Column(db.JSON, nullable=False)
-    bounding_box = db.Column(db.JSON, nullable=False)
-    grid_mapping = db.Column(db.JSON, nullable=False)
+    bounding_box = db.Column(db.JSON)
+    grid_mapping = db.Column(db.JSON)
     product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'), nullable=False)
 
     def __repr__(self):
@@ -65,21 +68,42 @@ class Domain(db.Model):
 
 
 class Horizon(db.Model):
+    __table_args__ = (
+        db.UniqueConstraint('horizon', 'product_id', name='horizon_uc'),
+    )
     horizon_id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'), nullable=False)
     horizon = db.Column(db.Integer, nullable=False)
+
 
     def __repr__(self):
         return '<Horizon {!r}>'.format(self.horizon_id)
 
 
 class Issue(db.Model):
+    __table_args__ = (
+        db.UniqueConstraint('issue', 'product_id', name='issue_uc'),
+    )
     issue_id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'), nullable=False)
     issue = db.Column(db.Time, nullable=False)
 
     def __repr__(self):
         return '<Issue {!r}>'.format(self.issue_id)
+
+
+class NCFile(db.Model):
+    __table_args__ = (
+        db.UniqueConstraint('file_name', 'product_id', name='nc_file_uc'),
+    )
+    file_id = db.Column(db.Integer, primary_key=True)
+    file_name = db.Column(db.String, nullable=False)
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'), nullable=False)
+
+    def __repr__(self):
+        return '<NCFile {!r}>'.format(self.file_id)
 
 
 class ProductSchema(ma.ModelSchema):
---
+++ cuizinart/monitor.py
@@ -0,0 +1,105 @@
+import time
+import os
+from datetime import datetime
+from metadata_schema import *
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+from extract_netcdf_header import *
+
+
+def parse_str_time(strs):
+    return datetime.strptime(strs, '%Y-%m-%d %H:%M:%S')
+
+
+def add_metadata(src_path):
+    product_name = src_path.split('/')[-2]
+    file_name = src_path.split('/')[-1]
+    is_forecast = False
+    if product_name.startswith("forecast_"): # Folder name starts with forecast is forecast files
+        product_name = product_name[9:]
+        is_forecast = True
+
+    product = Product.query.filter_by(key=product_name).first()
+    if product is None:
+        print("Only support existing products")
+    else:
+        try:
+            f = Dataset(src_path,'r')
+        except Exception as e:
+            print('Missing File {}'.format(src_path))
+            raise e
+
+        times = parse_time(f)
+        if parse_str_time(times[0]) < product.start_date:
+            product.start_date = times[0]
+        if parse_str_time(times[-1]) > product.end_date:
+            product.end_date = times[-1]
+        new_file = NCFile(file_name=file_name, start_date=times[0], end_date=times[-1], product=product)
+        db.session.add(new_file)
+        if is_forecast:
+            issue = file_name[-7:-3]
+            if Issue.query.filter_by(issue=issue).first() is None:
+                new_issue = Issue(issue=issue, product=product)
+                db.session.add(new_issue)
+            for t in times:
+                t = parse_str_time(t)
+                if Horizon.query.filter_by(horizon=t.hour).first() is None:
+                    db.session.add(Horizon(horizon=t.hour, product=product))
+
+        db.session.add(product)
+        db.session.commit()
+
+
+class MonitorHandler(PatternMatchingEventHandler):
+    def on_created(self, event):
+        # This function is called when a file is created
+        db.session.remove()
+        add_metadata(event.src_path)
+
+        print("event path: {} , event type: {}, is Dir: {}!".format(event.src_path, event.event_type, event.is_directory))
+
+
+    def on_deleted(self, event):
+        # This function is called when a file is deleted
+        print("Someone deleted {}!".format(event.src_path))
+
+
+def update_existing_file_changes(prod_path):
+    db.session.remove()
+    all_nc_files = NCFile.query.all()
+    file_names = list(map(lambda f: f.file_name, all_nc_files))
+    for (path, subdirs, files) in os.walk(prod_path):
+        if path != products_path:
+            f_names = list(filter(lambda n: n.endswith('.nc'), files))
+            added_files, deleted_files = list(set(f_names) - set(file_names)), list(set(file_names) - set(f_names))
+            for name in added_files:
+                print("New added file {}".format(os.path.join(path, name)))
+                add_metadata(os.path.join(path, name))
+            for name in deleted_files:
+                print("Deleted {}".format(os.path.join(path, name)))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        products_path = sys.argv[1]
+    else:
+        print('Usage {} product_path'.format(sys.argv[0]))
+        LOGGER.error('Invalid argument')
+        sys.exit(1)
+
+    print("path: {}".format(products_path))
+
+    update_existing_file_changes(products_path)
+
+    event_handler = MonitorHandler(patterns=["*.nc"],
+                                   ignore_patterns=[],
+                                   ignore_directories=True)
+    observer = Observer()
+    observer.schedule(event_handler, products_path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(5)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
