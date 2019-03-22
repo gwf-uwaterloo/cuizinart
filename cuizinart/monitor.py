42be076aeb1b9e89d1ca6e1730fa068bbe65e60d
Yixin Wang <ywang@sprypoint.com>
2019-03-22 15:16:11 -0400

    minor fix for horizon

--- cuizinart/monitor.py
+++ cuizinart/monitor.py
@@ -12,8 +12,8 @@ def parse_str_time(strs):
 
 
 def add_metadata(src_path):
-    product_name = src_path.split('/')[-2]
-    file_name = src_path.split('/')[-1]
+    product_name = os.path.basename(os.path.dirname(src_path))
+    file_name = os.path.basename(src_path)
     is_forecast = False
     if product_name.startswith("forecast_"): # Folder name starts with forecast is forecast files
         product_name = product_name[9:]
@@ -41,10 +41,12 @@ def add_metadata(src_path):
             if Issue.query.filter_by(issue=issue).first() is None:
                 new_issue = Issue(issue=issue, product=product)
                 db.session.add(new_issue)
+            start_time = parse_str_time(times[0])
             for t in times:
                 t = parse_str_time(t)
-                if Horizon.query.filter_by(horizon=t.hour).first() is None:
-                    db.session.add(Horizon(horizon=t.hour, product=product))
+                horizon = (t - start_time).days*24 + (t - start_time).seconds/3600 # hourly
+                if Horizon.query.filter_by(horizon=horizon).first() is None:
+                    db.session.add(Horizon(horizon=horizon, product=product))
 
         db.session.add(product)
         db.session.commit()
