30f4900c46d037d1ca6bcc5732196e126bfbfaac
gauchm <martin.gauch@uwaterloo.ca>
2019-04-25 17:19:43 -0400

    Setup dev instance

--- cuizinart/docker-entrypoint.sh
+++ cuizinart/docker-entrypoint.sh
@@ -6,4 +6,4 @@ flask pyspark-init ${CUIZINART_PYSPARK_PASSWORD}
 
 cd cuizinart
 python3 monitor.py ${NC_INPUT_PATH} &
-uwsgi -s 0.0.0.0:5000 --manage-script-name --mount /cuizinart=main:app --master --processes 4 --threads 2 --logto ${LOG_DIRECTORY}/cuizinart_uwsgi.log --logfile-chmod 666
+uwsgi --http-socket 0.0.0.0:5000 --manage-script-name --mount /cuizinart=main:app --master --processes 4 --threads 2 --logto ${LOG_DIRECTORY}/cuizinart_uwsgi.log --logfile-chmod 666
