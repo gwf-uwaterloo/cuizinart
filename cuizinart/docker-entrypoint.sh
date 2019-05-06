d98b9e3b699e8da70e24ae3e75a8018dab51529f
Martin Gauch <15731649+gauchm@users.noreply.github.com>
2019-05-06 15:42:56 -0700

    Fix uwsgi<->nginx communication (#79)

--- cuizinart/docker-entrypoint.sh
+++ cuizinart/docker-entrypoint.sh
@@ -13,4 +13,4 @@ flask pyspark-init ${CUIZINART_PYSPARK_PASSWORD}
 
 cd cuizinart
 python3 monitor.py ${NC_INPUT_PATH} &
-uwsgi --http-socket 0.0.0.0:5000 --manage-script-name --mount /cuizinart=main:app --master --processes 4 --threads 2 --logto ${LOG_DIRECTORY}/cuizinart_uwsgi.log --logfile-chmod 666
+uwsgi --socket 0.0.0.0:5000 --manage-script-name --mount /cuizinart=main:app --master --processes 4 --threads 2 --logto ${LOG_DIRECTORY}/cuizinart_uwsgi.log --logfile-chmod 666
