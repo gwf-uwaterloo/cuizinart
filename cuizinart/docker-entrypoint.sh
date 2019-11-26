ffc28526e2e3f6cc813d9dfca5d4e4f0a50aba1e
raidraptor <adept2015@hotmail.com>
2019-11-27 07:21:54 +0900

    Fix temp dir (#125)
    * Added tmp folder for non root access

--- cuizinart/docker-entrypoint.sh
+++ cuizinart/docker-entrypoint.sh
@@ -1,13 +1,13 @@
-if [ ! -f /home/gwf/migrations/env.py  ]
+if [ ! -f /tmp/gwf/migrations/env.py  ]
 then
     echo "db init"
-    flask db init --directory /home/gwf/tmp_migration  # Can't init on an existing empty folder, so we init on a temporary one and then move contents to the mounted volume
-    mv /home/gwf/tmp_migration/* /home/gwf/migrations/  # Move content to mounted volume so it persists across builds
-    rm -rf /home/gwf/tmp_migration
+    flask db init --directory /tmp/gwf/migrations/tmp_migration  # Can't init on an existing empty folder, so we init on a temporary one and then move contents to the mounted volume
+    mv /tmp/gwf/migrations/tmp_migration/* /tmp/gwf/migrations/  # Move content to mounted volume so it persists across builds
+    rm -rf /tmp/gwf/migrations/tmp_migration
 fi
 
-flask db migrate
-flask db upgrade
+flask db migrate --directory /tmp/gwf/migrations
+flask db upgrade --directory /tmp/gwf/migrations
 
 flask pyspark-init ${CUIZINART_PYSPARK_PASSWORD}
 
