33271c63ea0d97552289893baa6ada0cffa7c079
raidraptor <adept2015@hotmail.com>
2020-03-08 12:04:16 -0400

    Update dbBuild.py

--- cuizinart/dbBuild.py
+++ cuizinart/dbBuild.py
@@ -1,7 +1,28 @@
-from metadata_schema import ProductSchema, Product, Domain,Horizon,Issue, Request,Variable, db
 import csv
+from metadata_schema import ProductSchema, Product, Domain,Horizon,Issue, Request,Variable, db
 
+
+m={}
 with open ("Tbl_prod_backup.csv") as csvfile:
     reader=csv.reader(csvfile)
     for row in reader:
-        print(row)
+        prod=Product(product_id=row['prodkey'],key=row['prod'],name=row['product_name'],start_date=row['startdate'],end_date=row['enddate'])
+        m[row['product_id']]=row['prod']
+with open("Tbl_horizon_backup.csv") as csvfile:
+    reader=csv.reader(csvfile)
+    for row in reader:
+        horizon=Horizon(horizon_id=row['horizonkey'],horizon=row["avail_horizon"])
+
+with open("Tbl_issues_backup.csv") as csvfile:
+    reader=csv.reader(csvfile)
+    for row in reader:
+        issue=Issue(issue_id=row['issuekey'],issue=row["avail_issues"])
+
+with open("Tbl_grid_user.csv") as csvfile:
+    reader=csv.reader(csvfile)
+    for row in reader:
+        user= User(id=row["userkey"],email=row['email'],first_name=row['firstname'],last_name=row['lastname'],organization=row['employer'],globus_id=row['globus_id'])
+
+# with open("Tbl_vbl_backup") as csvfile:
+#     reader=csv.reader(csvfile)
+#     for row in reader:
