75e564bec524166b5ad146667c23f15b237c7e19
raidraptor <adept2015@hotmail.com>
2020-03-11 00:30:58 -0400

    Update dbBuild.py

--- cuizinart/dbBuild.py
+++ cuizinart/dbBuild.py
@@ -1,6 +1,6 @@
 import csv
 from settings import app
-from metadata_schema import ProductSchema, Product, Domain,Horizon,Issue, Request,Variable, db
+from metadata_schema import ProductSchema, Product, Domain,Horizon,Issue, Request,Variable,User, db
 
 
 m={}
@@ -10,24 +10,38 @@ def db_build():
         reader=csv.DictReader(csvfile)
         for row in reader:
             prod=Product(product_id=row['prodkey'],key=row['prod'],name=row['product_name'],start_date=row['startdate'],end_date=row['enddate'])
-            m[row['product_id']]=row['prod']
+            prod.horizons=[]
+            prod.issues=[]
+            prod.variables=[]
+            m[row['product_id']]=prod
             db.session.add(prod)
-    db.session.commit()
-#     with open("Tbl_horizon_backup.csv") as csvfile:
-#         reader=csv.DictReader(csvfile)
-#         for row in reader:
-#             horizon=Horizon(horizon_id=row['horizonkey'],horizon=row["avail_horizon"])
+    
 
-#     with open("Tbl_issues_backup.csv") as csvfile:
-#         reader=csv.DictReader(csvfile)
-#         for row in reader:
-#             issue=Issue(issue_id=row['issuekey'],issue=row["avail_issues"])
-
-#     with open("Tbl_grid_user.csv") as csvfile:
-#         reader=csv.DictReader(csvfile)
-#         for row in reader:
-#             user= User(id=row["userkey"],email=row['email'],first_name=row['firstname'],last_name=row['lastname'],organization=row['employer'],globus_id=row['globus_id'])
-
-    # with open("Tbl_vbl_backup") as csvfile:
+    with open("DB_Backup/Tbl_horizon_backup.csv") as csvfile:
+        reader=csv.DictReader(csvfile)
+        for row in reader:
+            horizon=Horizon(horizon_id=row['horizonkey'],horizon=row["avail_horizon"])
+            m[row['product_id']].horizons.append(horizon)
+            db.session.add(horizon)
+    with open("DB_Backup/Tbl_issues_backup.csv") as csvfile:
+        reader=csv.DictReader(csvfile)
+        for row in reader:
+            issue=Issue(issue_id=row['issuekey'],issue=row["avail_issues"])
+            m[row['product_id']].issue.append(issue)
+            db.session.add(issue)
+    with open("DB_Backup/Tbl_grid_user.csv") as csvfile:
+        reader=csv.DictReader(csvfile)
+        for row in reader:
+            user= User(id=row["userkey"],email=row['email'],first_name=row['firstname'],last_name=row['lastname'],organization=row['employer'],globus_id=row['globus_id'])
+            db.session.add(user)
+    with open("DB_Backup/Tbl_vbl_backup") as csvfile:
+        reader=csv.DictReader(csvfile)
+        for row in reader:
+            var=Variable(variable_id=row["vblkey"],key=row["vbl"],name=["vbl_name"],unit=row["units"],ec_varname=row["ec_vname"],level=["level"],is_live=row['islive'],type=row['type'])
+            m[row['product_id']].variables.append(var)
+            db.session.add(var)
+    # with open("DB_Backup/Tbl_request_backup") as csvfile:
     #     reader=csv.DictReader(csvfile)
     #     for row in reader:
+    #         req=Request(request_id=row['requestkey'],request_name=row["request_id"],)
+    db.session.commit()
