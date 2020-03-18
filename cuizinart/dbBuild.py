b038261bf4a20c7406636c50bdc7bc620f0dad71
raidraptor <adept2015@hotmail.com>
2020-03-18 14:45:22 -0400

    Update request reading
    As well as small ui change

--- cuizinart/dbBuild.py
+++ cuizinart/dbBuild.py
@@ -2,9 +2,15 @@ import csv
 from settings import app
 from metadata_schema import ProductSchema, Product, Domain,Horizon,Issue, Request,Variable,User, db
 from datetime import datetime, timedelta, time, timezone
+import json
+import sys
 
+csv.field_size_limit(sys.maxsize)
 
 m={}
+usermap={}
+pmap={}
+emails=set()
 @app.cli.command()
 def db_build():
     with open ("DB_Backup/Tbl_prod_backup.csv") as csvfile:
@@ -14,7 +20,10 @@ def db_build():
             prod.horizons=[]
             prod.issues=[]
             prod.variables=[]
+            prod.requests=[]
+            prod.domain=Domain(extent=json.loads('{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[-83.3203125,43.068887774169625],[-73.47656249999999,43.068887774169625],[-73.47656249999999,48.922499263758255],[-83.3203125,48.922499263758255],[-83.3203125,43.068887774169625]]]}}]}'))
             m[row['product_id']]=prod
+            pmap[row['prod']]=prod
             db.session.add(prod)
     
 
@@ -33,16 +42,42 @@ def db_build():
     with open("DB_Backup/Tbl_user_backup.csv") as csvfile:
         reader=csv.DictReader(csvfile)
         for row in reader:
-            user= User(id=int(row["userkey"]),email=row['email'],first_name=row['firstname'],last_name=row['lastname'],organization=row['employer'],globus_id=row['globus_id'])
-            db.session.add(user)
+            if(not (row['email'] in emails)):
+                user= User(id=int(row["userkey"]),email=row['email'],first_name=row['firstname'],last_name=row['lastname'],organization=row['employer'],globus_id=row['globus_id'])
+                user.requests=[]
+                emails.add(row['email'])
+                db.session.add(user)
+                usermap[row['uid']]=user
+            
     with open("DB_Backup/Tbl_vbl_backup.csv") as csvfile:
         reader=csv.DictReader(csvfile)
         for row in reader:
             var=Variable(variable_id=int(row["vblkey"]),key=row["vbl"],name=["vbl_name"],unit=row["units"],ec_varname=row["ec_vname"],level=["level"],is_live=(row['islive']=='t'),type=row['type'])
             m[row['product_id']].variables.append(var)
             db.session.add(var)
-    # with open("DB_Backup/Tbl_request_backup") as csvfile:
-    #     reader=csv.DictReader(csvfile)
-    #     for row in reader:
-    #         req=Request(request_id=row['requestkey'],request_name=row["request_id"],)
+    with open("DB_Backup/Tbl_request_backup.csv") as csvfile:
+        reader=csv.DictReader(csvfile)
+        for row in reader:
+            req=Request(request_id=row['requestkey'],request_name=row["request_id"],request_valid=(row['valid']=='t'),file_location=row['file_location'],request_status=row['request_status'])
+            if(row['processing_time_s']!=""):
+                req.processing_time_s=int(row["processing_time_s"])
+            if(row['file_size_mb']!=""):
+                req.file_size_mb=int(row['file_size_mb'])
+            if(row["processed_stat"]!=""):
+                req.processed_stat=int(row["processed_stat"])
+            if(row['sent'] != ""):
+                if(len(row['sent'])<20):
+                    req.received_time=datetime.strptime(row['sent'],'%Y-%m-%d %H:%M:%S')
+                else:
+                    req.received_time=datetime.strptime(row['sent'],'%Y-%m-%d %H:%M:%S.%f')
+            if(row["processed"]!=""):
+                req.processed_time=datetime.strptime(row["processed"],'%Y-%m-%d %H:%M:%S.%f')
+            if(row['email_sent']!=""):
+                req.email_sent_time=datetime.strptime(row['email_sent'],'%Y-%m-%d %H:%M:%S.%f')
+            if(row['req_str']!=""):
+                req.request_json=json.loads(row['req_str'])
+            if(row['uid'] in usermap and row['prod'] in pmap):
+                usermap[row['uid']].requests.append(req)
+                pmap[row['prod']].requests.append(req)
+                db.session.add(req)
     db.session.commit()
--- cuizinart/frontend/src/components/About.jsx
+++ cuizinart/frontend/src/components/About.jsx
@@ -61,9 +61,11 @@ export default class About extends Component {
                             <a href="mailto:caspar.data@uwaterloo.ca">caspar.data@uwaterloo.ca</a><br/>
                             <a className="ml-auto" href="http://www.caspar-data.ca" target="_blank"
                                rel="noopener noreferrer">http://www.caspar-data.ca</a>
+                            <hr />
                             <h5>Citation</h5>
                             <p>
-                            Mai et al. (2019).<br/> The Canadian Surface Prediction Archive (CaSPAr): A Platform to Enhance Environmental Modeling in Canada and Globally.Bulletin of the American Meteorological Society, https://doi.org/10.1175/BAMS-D-19-0143.1.
+                            Mai et al. (2019).<br/> The Canadian Surface Prediction Archive (CaSPAr): A Platform to Enhance Environmental Modeling in Canada and Globally. Bulletin of the American Meteorological Society, <a className="ml-auto" href="http://www.caspar-data.ca" target="_blank" 
+                                rel="noopener noreferrer">https://doi.org/10.1175/BAMS-D-19-0143.1.</a>
                             </p>
                         </CardContent>
                     </Card>
