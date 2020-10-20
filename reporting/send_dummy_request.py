f070035428724d89f7d5dac13f10a5f77dae8ed4
Martin Gauch <martin.gauch@uwaterloo.ca>
2020-10-20 13:22:05 +0200

    Add fcst_window and change dates in dummy request

--- reporting/send_dummy_request.py
+++ reporting/send_dummy_request.py
@@ -29,6 +29,6 @@ if __name__ == '__main__':
     token = result["response"]["user"]["authentication_token"]
 
     now = time.time()
-    json_request = '{{"variables": ["CaPA_coarse_A_PR_SFC"], "fcst_window": [], "issues": ["00:00"], "product": "CaPA_coarse", "backend": "slurm", "bounding_geom": [{{"type": "Feature", "properties": {{}}, "geometry": {{"type": "Polygon", "coordinates": [[[-94.956207, 34.03872], [-94.956207, 34.039858], [-94.954491, 34.039858], [-94.954491, 34.03872], [-94.956207, 34.03872]]]}}}}], "start_time": "2012-09-17", "end_time": "2012-09-17", "request_id": "{}_{}", "user_email": "{}", "user_firstname": "Caspar", "user_lastname": "Dummy", "globus_id": "julemai@computecanada.ca"}}'.format(email, now, email)
+    json_request = '{{"variables": ["CaPA_coarse_A_PR_SFC"], "fcst_window": [0], "issues": ["00:00"], "product": "CaPA_coarse", "backend": "slurm", "bounding_geom": [{{"type": "Feature", "properties": {{}}, "geometry": {{"type": "Polygon", "coordinates": [[[-94.956207, 34.03872], [-94.956207, 34.039858], [-94.954491, 34.039858], [-94.954491, 34.03872], [-94.956207, 34.03872]]]}}}}], "start_time": "2012-09-17", "end_time": "2012-09-18", "request_id": "{}_{}", "user_email": "{}", "user_firstname": "Caspar", "user_lastname": "Dummy", "globus_id": "julemai@computecanada.ca"}}'.format(email, now, email)
 
     post_request(post_url, token, json.loads(json_request))
